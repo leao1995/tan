1f2c21e7af756578a650355da12a8ecc4c9442d4
leao1995 <30990732+leao1995@users.noreply.github.com>
2019-07-17 16:16:35 -0400

    change uci dataset to match with vaeac settings

--- data/uci/prepare_data.py
+++ data/uci/prepare_data.py
@@ -1,7 +1,12 @@
 import os
+import sys
+sys.path.insert(0, '/nas/longleaf/home/yangli95/Workspace/TAN/tan')
 import numpy as np
 import pandas as pd
 import pickle
+
+from tan.data import hepmass
+
 
 path = './original_data'
 save_path = '.'
@@ -11,9 +16,9 @@ def split(data, name):
     data = data.copy()
     np.random.shuffle(data)
     N = data.shape[0]
-    train = data[:int(N * 0.6)]
-    valid = data[int(N * 0.6):int(N * 0.8)]
-    test = data[int(N * 0.8):]
+    train = data[:int(N * 0.85)]
+    valid = data[int(N * 0.85):N]
+    test = data
 
     dataset = {
         'train': train,
@@ -27,4 +32,7 @@ def split(data, name):
 # white wine
 data = pd.read_csv(os.path.join(path, 'winequality-white.csv'), sep=';')
 data = np.array(data)
-split(data, 'white')
+split(data, 'white_vaeac')
+
+# hepmass
+# hepmass.download_and_make_data(path)
